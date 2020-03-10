0e6f315fda0a18912d5528f8dada7656c4a4845c
Vanna Sok <53488795+vannasok@users.noreply.github.com>
2020-03-10 10:09:16 -0700

    task7-9

--- src/03_modules.py
+++ src/03_modules.py
@@ -13,7 +13,10 @@ import getpass
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-print(sys.argv)
+# print(sys.argv[0])
+for x in sys.argv:
+    print(x)
+
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
@@ -22,7 +25,7 @@ print(sys.platform)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-print(sys.version_info)
+print('sys version: ' + str(sys.version_info))
 
 
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
